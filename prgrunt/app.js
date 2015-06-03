205b6893f47f1bfdc6af0d887ce27813937a1410
nicolads87 <nicola.ds87@gmail.com>
2015-06-03 09:11:53 +0200

    created module for socket

--- prgrunt/app.js
+++ prgrunt/app.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var cluster = require('cluster');
-var worker = cluster.worker;
+
 var http = require('http');
 var middleware = require('./middleware.js');
-if (worker === null) {
-    worker = {id: process.pid};
-}
+
 app.use('/', middleware);
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('../frontend/views'));
@@ -23,24 +20,7 @@ app.use('/static', express.static('../frontend'));
 var clients = {}, numClients = 1;
 var server = http.createServer(app);
 var io = require('socket.io')(server);
-io.on('connection', function(socket) {
-    clients[numClients] = socket.id;
-    numClients = numClients + 1;
-    console.log('connection' + 'S:', socket.id + ' W:' + worker.id);
-    socket.on('disconnect', function() {
-        console.log("disconnect" + 'S:', socket.id + ' W:' + worker.id);
-    });
-    
-    socket.on('join', function(data) {
-        console.log('join ' + data.user + ' ' + data.to);
-        clients[data.user] = socket.id;
-    });
-    socket.on('gatto', function(message) {
-        console.log('Received on worker ' + worker.id + ' SocketId: ' + socket.id + ' ' + message.user + ': ' +message.message);
-        //io.emit('gatto', message);
-        io.sockets.connected[clients[message.to]].emit('gatto', message);
-    });
-});
+require('./socket.js')(io);
 
 module.exports = {
     server: server
---
+++ prgrunt/socket.js
@@ -0,0 +1,32 @@
+var cluster = require('cluster');
+var worker = cluster.worker;
+
+if (worker === null) {
+    worker = {id: process.pid};
+}
+
+var clients = {}, numClients = 1;
+function initSocket(io) {
+    io.on('connection', function(socket) {
+        clients[numClients] = socket.id;
+        numClients = numClients + 1;
+        console.log('connection' + 'S:', socket.id + ' W:' + worker.id);
+        socket.on('disconnect', function() {
+            console.log("disconnect" + 'S:', socket.id + ' W:' + worker.id);
+        });
+
+        socket.on('join', function(data) {
+            console.log('join ' + data.user + ' ' + data.to);
+            clients[data.user] = socket.id;
+        });
+        socket.on('gatto', function(message) {
+            console.log('Received on worker ' + worker.id + ' SocketId: ' + socket.id + ' ' + message.user + ': ' +message.message);
+            //io.emit('gatto', message);
+            io.sockets.connected[clients[message.to]].emit('gatto', message);
+        });
+    });
+    
+}
+module.exports = initSocket;
+
+
