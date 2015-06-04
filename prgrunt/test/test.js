57df5d8b20222cfd07acb7190844002cf88a368d
nicolads87 <nicola.ds87@gmail.com>
2015-06-04 21:08:19 +0200

    test login
    added module q

--- prgrunt/test/test.js
+++ prgrunt/test/test.js
@@ -1,6 +1,18 @@
-var assert = require("chai").assert; // The Assert API covers the TDD assertion style.
-var chai = require("chai");
-chai.should(); // The Expect / Should API covers the BDD assertion styles.
+// The Assert API covers the TDD assertion style.
+var chai = require("chai"), chaiHttp = require('chai-http');
+var assert = chai.assert;
+var expect = chai.expect;
+// The Expect / Should API covers the BDD assertion styles.
+chai.use(chaiHttp);
+chai.should();
+var url = 'http://localhost:3000';
+var app = require('../app.js').server;
+// Add promise support if this does not exist natively.
+if (!global.Promise) {
+  var q = require('q');
+  chai.request.addPromises(q.Promise);
+}
+
 
 var objToTest = {
     name: "camoscio"
@@ -28,4 +40,25 @@ describe('#objToTest', function() {
         objToTest.should.have.property('name');
     });*/
 
+});
+
+describe('Login', function() {
+    
+    it('shoud have status 200', function(done) {
+        //not work with .then use .end
+        chai.request(app).get('/login').end(function(res) {
+            expect(res).to.have.status(200);
+            done();
+        });
+            
+    });
+    
+    it('shoud be html', function(done) {
+        chai.request(app).get('/login').end(function(res) {
+            expect(res).to.be.html;
+            done();
+        });
+            
+    });
+    
 });
