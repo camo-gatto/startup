8f85b2c83ff1aa907709f11fda8cf6470726e7e0
nicolads87 <nicola.ds87@gmail.com>
2015-08-19 21:52:28 +0200

    example unit test

---
+++ app/frontend/app/test/test.js
@@ -0,0 +1,10 @@
+define(['mod'], function (modulo) {
+
+    describe('Unit testing mod', function() {
+
+      it('mod color is black', function() {
+        expect(modulo.color).toBe('black');
+      });
+    });
+
+});
